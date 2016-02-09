b0938d4789cbadb55d34f5ae95afc819fc9d68b8
maggie27 <magdelenewilliam@gmail.com>
2016-02-10 04:26:37 +0800

    Create pharmacist.php

---
+++ pharmacist.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+include_once('connect_db.php');
+if(isset($_SESSION['username'])){
+$id=$_SESSION['pharmacist_id'];
+$fname=$_SESSION['first_name'];
+$lname=$_SESSION['last_name'];
+$sid=$_SESSION['staff_id'];
+$user=$_SESSION['username'];
+
+}else{
+header("location:http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/index.php");
+exit();
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php echo $user;?> - Pharmaceutical Inventory System</title>
+<link rel="stylesheet" type="text/css" href="style/mystyle.css">
+<link rel="stylesheet" href="style/style.css" type="text/css" media="screen" /> 
+<link rel="stylesheet" type="text/css" href="style/dashboard_styles.css"  media="screen" />
+<script src="js/function.js" type="text/javascript"></script>
+<style>
+#left_column{
+height: 470px;
+}
+</style>
+</head>
+<body>
+<div id="content">
+<div id="header">
+<h1><a href="#"><img src="images/hd_logo.jpg"></a> Pharmaceutical Inventory System</h1></div>
+<div id="left_column">
+<div id="button">
+<ul>
+			<li><a href="pharmacist.php">Dashboard</a></li>
+			<li><a href="prescription.php">Prescription</a></li>
+			<li><a href="stock_pharmacist.php">Stock</a></li>
+			<li><a href="logout.php">Logout</a></li>
+		</ul>	
+</div>
+</div>
+
+</div>
+</body>
+</html>
