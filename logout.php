5e001369c75f9fd553e5dd02bbafa0dd16b093bb
CavinMichael <1112700763@student.mmu.edu.my>
2016-02-14 00:52:22 +0800

    logout

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+unset($_SESSION['pharmacist_id']);
+unset($_SESSION['first_name']);
+unset($_SESSION['last_name']);
+unset($_SESSION['staff_id']);
+unset($_SESSION['username']);
+header("location:http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/index.php");
+?>
