e9f7606f03b0297cd8f06134bbdd06c4cea83d33
Dharma93 <1141125271@student.mmu.edu.my>
2016-02-15 15:14:33 +0800

    Update search.php

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <?php 
 ob_start();
-require_once("connect_db.php"); ?>
+include_once("connect_db.php"); ?>
 
 <!DOCTYPE html>
 <html>
@@ -24,14 +24,14 @@ height: 470px;
 <div id="button">
 <ul>
 			<li><a href="pharmacist.php">Dashboard</a></li>
-			<li><a href="prescription.php">Prescription</a></li>
+			<li><a href="sales.php">Sales</a></li>
 			<li><a href="stock_pharmacist.php">Stock</a></li>
 			<li><a href="logout.php">Logout</a></li>
 </ul>	
    </div>
 </div>
 
-<td height="41" bgcolor="#CCCCCC"><form name="search" method="post" action="search.php">
+<td height="41" bgcolor="#CCCCCC"><form name="search" method="post" action="search2.php">
 <input type="text"  maxlength="40" name="find" style="border-bottom-left-radius:10; border-bottom-right-radius:10; border-top-left-radius:10; border-top-right-radius:10" />
 <input name="search" type="submit" id="search" style="background-color:#09F; font-family:Verdana, Geneva, sans-serif; font-weight:bold; border-bottom-left-radius:10px; border-bottom-right-radius:10px; border-top-left-radius:10px; border-bottom-right-radius:10px" value="Go" />
 </form>
@@ -62,18 +62,18 @@ height: 470px;
 </table>
                        
 <?php
-	//declaring my input text box
 	$input = $_POST['find'];
+
 	if($input=="")
 	{
-		echo"<p><h3>You forgot to enter your search term!</h3>";
+		echo"<p><h3>Input in search Medicine ID or Name </h3>";
 		exit;
-	}
+	
 		
 
 	$sql2 = "select * from stock where stock_id LIKE '%$input%' OR drug_name LIKE '%$input%'";
-	$result2 = mysql_query($sql2) or die(mysql_error());
-	
+	$result2 = mysql_query($sql2) or die("Unavailable in database");
+	}
 
 while($row = mysql_fetch_array($result2))
   {
@@ -83,49 +83,5 @@ while($row = mysql_fetch_array($result2))
 	  $quantity = $row['quantity'];
 	  $sold = $row['sold'];
 
-	  
-  echo "
-  <table width=\"600\" align=\"center\">
-      
-<tr>
-
-<td width=\"270\" align=\"center\" height=\"30\" style=\"font-family:Verdana, Geneva, sans-serif; 
-font-size:12px; font-weight:bold; 
-bgcolor=\"#E0E0E0\"><a href=\"search1.php?stockid=$stockid\" class=\"style1\">$stockid</td>
-     
-<td width=\"270\" align=\"center\" height=\"30\" style=\"font-family:Verdana, Geneva, sans-serif; 
-font-size:12px; font-weight:bold; 
-bgcolor=\"#E0E0E0\">$drugname</td>
-
-<td width=\"270\" align=\"center\" height=\"30\" style=\"font-family:Verdana, Geneva, sans-serif; 
-font-size:12px; font-weight:bold; 
-bgcolor=\"#E0E0E0\">$description</td>
-
-<td width=\"270\" align=\"center\" height=\"30\" style=\"font-family:Verdana, Geneva, sans-serif; 
-font-size:12px; font-weight:bold; 
-bgcolor=\"#E0E0E0\">$quantity</td>
-
-
-    </table> ";
-  }
-  
-
-
-?>
- 
-<!DOCTYPE html>
-<html>
-<body>
-
-<form action="#">
-Sold <input type="text"><br>
-<input type="submit" value="Submit">
-</form>
-</body>
-</html>
-
-
-
-</script>
-</body>
-</html>
+  } 
+ ?>
