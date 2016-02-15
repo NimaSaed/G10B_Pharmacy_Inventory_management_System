ab24b7ead874356225a340110ee52d05b6379a2d
Dharma93 <dharmar508@gmail.com>
2016-02-15 15:45:35 +0800

    :D

--- pharmacy.sql
+++ pharmacy.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Dec 14, 2013 at 09:55 AM
--- Server version: 5.6.12-log
--- PHP Version: 5.4.16
+-- Host: 127.0.0.1
+-- Generation Time: Feb 15, 2016 at 08:39 AM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.5.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,62 +14,11 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `pharmacy`
 --
-CREATE DATABASE IF NOT EXISTS `pharmacy` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `pharmacy`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
-
-CREATE TABLE IF NOT EXISTS `admin` (
-  `admin_id` tinyint(5) NOT NULL AUTO_INCREMENT,
-  `username` varchar(10) NOT NULL,
-  `password` varchar(10) NOT NULL,
-  `date` datetime NOT NULL,
-  PRIMARY KEY (`admin_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`admin_id`, `username`, `password`, `date`) VALUES
-(1, 'admin', 'admin', '0000-00-00 00:00:00');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cashier`
---
-
-CREATE TABLE IF NOT EXISTS `cashier` (
-  `cashier_id` tinyint(5) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(15) NOT NULL,
-  `last_name` varchar(15) NOT NULL,
-  `staff_id` varchar(10) NOT NULL,
-  `postal_address` varchar(20) NOT NULL,
-  `phone` varchar(12) NOT NULL,
-  `email` varchar(20) NOT NULL,
-  `username` varchar(10) NOT NULL,
-  `password` varchar(10) NOT NULL,
-  `date` datetime NOT NULL,
-  PRIMARY KEY (`cashier_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
-
---
--- Dumping data for table `cashier`
---
-
-INSERT INTO `cashier` (`cashier_id`, `first_name`, `last_name`, `staff_id`, `postal_address`, `phone`, `email`, `username`, `password`, `date`) VALUES
-(4, 'gfhf', 'ewdsc', '67hhtf', '45 nhyfg', '65424579', 'will@henry.com', 'gty', 'getty', '2013-11-23 12:54:49'),
-(5, 'Sam', 'Osoro', 'Pharmacy/C', '76 nairobi', '09865468', 'samwel@pharmacy.com', 'sam', '1234', '2013-11-25 20:20:44');
 
 -- --------------------------------------------------------
 
@@ -77,13 +26,12 @@ INSERT INTO `cashier` (`cashier_id`, `first_name`, `last_name`, `staff_id`, `pos
 -- Table structure for table `invoice`
 --
 
-CREATE TABLE IF NOT EXISTS `invoice` (
+CREATE TABLE `invoice` (
   `invoice_id` int(5) NOT NULL,
   `customer_name` varchar(30) NOT NULL,
   `served_by` varchar(15) NOT NULL,
   `status` varchar(10) NOT NULL DEFAULT 'Unpaid',
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`invoice_id`)
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -105,13 +53,11 @@ INSERT INTO `invoice` (`invoice_id`, `customer_name`, `served_by`, `status`, `da
 --
 -- Triggers `invoice`
 --
-DROP TRIGGER IF EXISTS `tarehe`;
-DELIMITER //
-CREATE TRIGGER `tarehe` AFTER INSERT ON `invoice`
- FOR EACH ROW BEGIN
+DELIMITER $$
+CREATE TRIGGER `tarehe` AFTER INSERT ON `invoice` FOR EACH ROW BEGIN
      SET @date=NOW();
 END
-//
+$$
 DELIMITER ;
 
 -- --------------------------------------------------------
@@ -120,16 +66,13 @@ DELIMITER ;
 -- Table structure for table `invoice_details`
 --
 
-CREATE TABLE IF NOT EXISTS `invoice_details` (
-  `id` tinyint(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `invoice_details` (
+  `id` tinyint(5) NOT NULL,
   `invoice` int(5) NOT NULL,
   `drug` tinyint(5) NOT NULL,
   `cost` int(5) DEFAULT NULL,
-  `quantity` int(5) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `stocks` (`drug`),
-  KEY `invoices` (`invoice`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=31 ;
+  `quantity` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `invoice_details`
@@ -165,41 +108,13 @@ INSERT INTO `invoice_details` (`id`, `invoice`, `drug`, `cost`, `quantity`) VALU
 -- --------------------------------------------------------
 
 --
--- Table structure for table `manager`
---
-
-CREATE TABLE IF NOT EXISTS `manager` (
-  `manager_id` tinyint(5) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(15) NOT NULL,
-  `last_name` varchar(15) NOT NULL,
-  `staff_id` varchar(10) NOT NULL,
-  `postal_address` varchar(20) NOT NULL,
-  `phone` varchar(12) NOT NULL,
-  `email` varchar(20) NOT NULL,
-  `username` varchar(10) NOT NULL,
-  `password` varchar(10) NOT NULL,
-  `date` datetime NOT NULL,
-  PRIMARY KEY (`manager_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `manager`
---
-
-INSERT INTO `manager` (`manager_id`, `first_name`, `last_name`, `staff_id`, `postal_address`, `phone`, `email`, `username`, `password`, `date`) VALUES
-(1, 'Samwel', 'Osoro', 'sam/pharm', '456 Kabu', '0789653417', 'samoso@pharmacy.com', 'samoso', '12345', '2013-12-10 14:09:03');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `paymenttypes`
 --
 
-CREATE TABLE IF NOT EXISTS `paymenttypes` (
-  `id` tinyint(5) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(15) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+CREATE TABLE `paymenttypes` (
+  `id` tinyint(5) NOT NULL,
+  `Name` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `paymenttypes`
@@ -218,8 +133,8 @@ INSERT INTO `paymenttypes` (`id`, `Name`) VALUES
 -- Table structure for table `pharmacist`
 --
 
-CREATE TABLE IF NOT EXISTS `pharmacist` (
-  `pharmacist_id` tinyint(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `pharmacist` (
+  `pharmacist_id` tinyint(5) NOT NULL,
   `first_name` varchar(15) NOT NULL,
   `last_name` varchar(15) NOT NULL,
   `staff_id` varchar(10) NOT NULL,
@@ -227,17 +142,19 @@ CREATE TABLE IF NOT EXISTS `pharmacist` (
   `phone` varchar(12) NOT NULL,
   `email` varchar(20) NOT NULL,
   `username` varchar(10) NOT NULL,
-  `password` varchar(10) NOT NULL,
   `date` datetime NOT NULL,
-  PRIMARY KEY (`pharmacist_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+  `password` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pharmacist`
 --
 
-INSERT INTO `pharmacist` (`pharmacist_id`, `first_name`, `last_name`, `staff_id`, `postal_address`, `phone`, `email`, `username`, `password`, `date`) VALUES
-(5, 'Sam', 'Osoro', 'Pharmacy/1', '56 Kabu', '0789653412', 'sam@pharmacysys.com', 'osoro', '1234', '2013-11-24 17:18:51');
+INSERT INTO `pharmacist` (`pharmacist_id`, `first_name`, `last_name`, `staff_id`, `postal_address`, `phone`, `email`, `username`, `date`, `password`) VALUES
+(1, 'kanimozhi', 'nalllathamby', 'Pharmacy2', '55100', '0909090909', 'kani@pharmacysys.com', 'kani', '2016-02-11 03:01:00', '1234'),
+(2, 'magdelene', 'william', '2', '55100', '08080808', 'mag@pharmacysys.com', 'mag', '2016-02-11 10:00:00', '1234'),
+(4, 'cavin', 'miachael', '4', '55100', '0303030303', 'cav@pharmacysys.com', 'cavin', '2016-02-11 11:00:00', '1234'),
+(5, 'dharma', 'seelan', 'Pharmacy1', '55100', '0789653412', 'd@pharmacysys.com', 'dharma', '2016-02-11 05:18:51', '1234');
 
 -- --------------------------------------------------------
 
@@ -245,8 +162,8 @@ INSERT INTO `pharmacist` (`pharmacist_id`, `first_name`, `last_name`, `staff_id`
 -- Table structure for table `prescription`
 --
 
-CREATE TABLE IF NOT EXISTS `prescription` (
-  `id` tinyint(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `prescription` (
+  `id` tinyint(5) NOT NULL,
   `prescription_id` int(5) NOT NULL,
   `customer_id` int(11) NOT NULL,
   `customer_name` varchar(30) NOT NULL,
@@ -255,9 +172,8 @@ CREATE TABLE IF NOT EXISTS `prescription` (
   `postal_address` varchar(20) NOT NULL,
   `invoice_id` tinyint(5) NOT NULL,
   `phone` varchar(12) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`,`prescription_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `prescription`
@@ -271,13 +187,11 @@ INSERT INTO `prescription` (`id`, `prescription_id`, `customer_id`, `customer_na
 --
 -- Triggers `prescription`
 --
-DROP TRIGGER IF EXISTS `taree`;
-DELIMITER //
-CREATE TRIGGER `taree` AFTER INSERT ON `prescription`
- FOR EACH ROW BEGIN
+DELIMITER $$
+CREATE TRIGGER `taree` AFTER INSERT ON `prescription` FOR EACH ROW BEGIN
 SET@date=NOW();
 END
-//
+$$
 DELIMITER ;
 
 -- --------------------------------------------------------
@@ -286,16 +200,14 @@ DELIMITER ;
 -- Table structure for table `prescription_details`
 --
 
-CREATE TABLE IF NOT EXISTS `prescription_details` (
-  `id` tinyint(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `prescription_details` (
+  `id` tinyint(5) NOT NULL,
   `pres_id` int(5) NOT NULL,
   `drug_name` tinyint(5) NOT NULL,
   `strength` varchar(15) NOT NULL,
   `dose` varchar(15) NOT NULL,
-  `quantity` int(5) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `dsfd` (`drug_name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=31 ;
+  `quantity` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `prescription_details`
@@ -334,15 +246,14 @@ INSERT INTO `prescription_details` (`id`, `pres_id`, `drug_name`, `strength`, `d
 -- Table structure for table `receipts`
 --
 
-CREATE TABLE IF NOT EXISTS `receipts` (
+CREATE TABLE `receipts` (
   `reciptNo` int(10) NOT NULL,
   `customer_id` varchar(10) NOT NULL,
   `total` int(10) NOT NULL,
   `payType` varchar(10) NOT NULL,
   `serialno` varchar(10) DEFAULT NULL,
   `served_by` varchar(15) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`reciptNo`)
+  `date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -356,14 +267,23 @@ INSERT INTO `receipts` (`reciptNo`, `customer_id`, `total`, `payType`, `serialno
 --
 -- Triggers `receipts`
 --
-DROP TRIGGER IF EXISTS `siku`;
-DELIMITER //
-CREATE TRIGGER `siku` AFTER INSERT ON `receipts`
- FOR EACH ROW BEGIN
+DELIMITER $$
+CREATE TRIGGER `siku` AFTER INSERT ON `receipts` FOR EACH ROW BEGIN
      SET @date=NOW();
 END
-//
+$$
 DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `soldmedic`
+--
+
+CREATE TABLE `soldmedic` (
+  `sold_date` date NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -371,8 +291,8 @@ DELIMITER ;
 -- Table structure for table `stock`
 --
 
-CREATE TABLE IF NOT EXISTS `stock` (
-  `stock_id` tinyint(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `stock` (
+  `stock_id` tinyint(5) NOT NULL,
   `drug_name` varchar(20) NOT NULL,
   `category` varchar(20) NOT NULL,
   `description` varchar(50) NOT NULL,
@@ -382,19 +302,22 @@ CREATE TABLE IF NOT EXISTS `stock` (
   `cost` int(11) NOT NULL,
   `status` enum('Available','Inavailable') NOT NULL,
   `date_supplied` date NOT NULL,
-  PRIMARY KEY (`stock_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+  `sold` int(11) NOT NULL,
+  `sales` varchar(15) NOT NULL,
+  `frequentlysold` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `stock`
 --
 
-INSERT INTO `stock` (`stock_id`, `drug_name`, `category`, `description`, `company`, `supplier`, `quantity`, `cost`, `status`, `date_supplied`) VALUES
-(5, 'Piriton', 'tablet', 'Painkiller', 'SB', 'SB', 1000, 5, 'Available', '2013-11-30'),
-(6, 'Dual Cotexin', 'tablet', 'Malaria', 'GX', 'Clinix', 150, 120, 'Available', '2013-11-30'),
-(7, 'Naproxen', 'Tablet', 'Reproductive', 'Family Health', 'Stopes', 250, 250, 'Available', '2013-11-30'),
-(8, 'Flagi', 'talet', 'Digestive', 'GX', 'Clinix', 657, 15, 'Available', '2013-11-30'),
-(9, 'Actal', 'Tablet', 'Stomach Reliev', 'GX', 'Clinix', 1000, 1, 'Available', '2013-12-06');
+INSERT INTO `stock` (`stock_id`, `drug_name`, `category`, `description`, `company`, `supplier`, `quantity`, `cost`, `status`, `date_supplied`, `sold`, `sales`, `frequentlysold`) VALUES
+(5, 'Piriton', 'tablet', 'Painkiller', 'SB', 'SB', 1000, 5, 'Available', '2013-11-30', 0, 'dailysales', 'yes'),
+(6, 'Dual Cotexin', 'tablet', 'Malaria', 'GX', 'Clinix', 150, 120, 'Available', '2013-11-30', 0, 'dailysales', 'no'),
+(7, 'Naproxen', 'Tablet', 'Reproductive', 'Family Health', 'Stopes', 250, 250, 'Available', '2013-11-30', 0, 'Monthlysales', 'no'),
+(8, 'Flagi', 'talet', 'Digestive', 'GX', 'Clinix', 657, 15, 'Available', '2013-11-30', 0, 'monthlysales', 'no'),
+(9, 'Actal', 'Tablet', 'Stomach Reliev', 'GX', 'Clinix', 1000, 1, 'Available', '2013-12-06', 0, 'Monthlysales', 'yes'),
+(10, 'Paracetamol', 'tablet', 'treats fever', 'Panadol', 'Panadol', 1000, 3, 'Available', '2016-02-11', 0, 'dailysales', 'yes');
 
 -- --------------------------------------------------------
 
@@ -402,8 +325,8 @@ INSERT INTO `stock` (`stock_id`, `drug_name`, `category`, `description`, `compan
 -- Table structure for table `tempprescri`
 --
 
-CREATE TABLE IF NOT EXISTS `tempprescri` (
-  `id` tinyint(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tempprescri` (
+  `id` tinyint(5) NOT NULL,
   `customer_id` int(11) NOT NULL,
   `customer_name` varchar(30) DEFAULT NULL,
   `age` int(11) DEFAULT NULL,
@@ -413,10 +336,109 @@ CREATE TABLE IF NOT EXISTS `tempprescri` (
   `drug_name` varchar(30) NOT NULL,
   `strength` varchar(30) NOT NULL,
   `dose` varchar(30) NOT NULL,
-  `quantity` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `quantity` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `invoice`
+--
+ALTER TABLE `invoice`
+  ADD PRIMARY KEY (`invoice_id`);
+
+--
+-- Indexes for table `invoice_details`
+--
+ALTER TABLE `invoice_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `stocks` (`drug`),
+  ADD KEY `invoices` (`invoice`);
+
+--
+-- Indexes for table `paymenttypes`
+--
+ALTER TABLE `paymenttypes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pharmacist`
+--
+ALTER TABLE `pharmacist`
+  ADD PRIMARY KEY (`pharmacist_id`);
+
+--
+-- Indexes for table `prescription`
+--
+ALTER TABLE `prescription`
+  ADD PRIMARY KEY (`id`,`prescription_id`);
+
+--
+-- Indexes for table `prescription_details`
+--
+ALTER TABLE `prescription_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `dsfd` (`drug_name`);
+
+--
+-- Indexes for table `receipts`
+--
+ALTER TABLE `receipts`
+  ADD PRIMARY KEY (`reciptNo`);
+
+--
+-- Indexes for table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`stock_id`);
+
+--
+-- Indexes for table `tempprescri`
+--
+ALTER TABLE `tempprescri`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `invoice_details`
+--
+ALTER TABLE `invoice_details`
+  MODIFY `id` tinyint(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+--
+-- AUTO_INCREMENT for table `paymenttypes`
+--
+ALTER TABLE `paymenttypes`
+  MODIFY `id` tinyint(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `pharmacist`
+--
+ALTER TABLE `pharmacist`
+  MODIFY `pharmacist_id` tinyint(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `prescription`
+--
+ALTER TABLE `prescription`
+  MODIFY `id` tinyint(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `prescription_details`
+--
+ALTER TABLE `prescription_details`
+  MODIFY `id` tinyint(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+--
+-- AUTO_INCREMENT for table `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `stock_id` tinyint(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `tempprescri`
+--
+ALTER TABLE `tempprescri`
+  MODIFY `id` tinyint(5) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
